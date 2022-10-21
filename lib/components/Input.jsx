2415a7bb39e416aa63616363f36f2d71bd920419
Emanuele Padovani <emanuele.padovani@kromin.it>
2022-10-21 12:45:13 +0200

    revert previous commit

--- lib/components/Input.jsx
+++ lib/components/Input.jsx
@@ -48,20 +48,16 @@ const TextareaField = styled.textarea`
   border-top: ${props => (props.invalid ? '0' : '1px solid #eee')};
   box-shadow: ${props => (props.invalid ? 'inset 0 0 2px #E53935' : 'none')};
   box-sizing: border-box;
-  background-color: ${props => (props.invalid ? '#DBDBDB' : 'none')};
   color: ${props => (props.invalid ? '#E53935' : '')};
   font-size: 16px;
   opacity: ${props => (props.disabled && !props.invalid ? '.5' : '1')};
   outline: none;
   padding: ${props => (props.hasButton ? '16px 52px 16px 10px' : '16px 10px')};
   width: 100%;
-  height: 54px;
-  resize: none;
   -webkit-appearance: none;
 
   &:disabled {
-    background: #dbdbdb;
-    cursor: not-allowed;
+    background: #fff;
   }
 
   @media screen and (max-width: 568px) {
